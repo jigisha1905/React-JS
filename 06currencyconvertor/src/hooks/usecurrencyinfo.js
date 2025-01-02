ccc06cef9b0dd03a076ee4900c49b0f0edb96edd
Jigisha Singh <rim2004jhim@gmail.com>
2025-01-02 10:21:32 +0530

    currency convertor project

---
+++ 06currencyconvertor/src/hooks/usecurrencyinfo.js
@@ -0,0 +1,18 @@
+import { useEffect,useState } from "react";
+
+//designing of custom hooks
+
+function usecurrencyinfo(currency){
+    const[data,setdata]=useState({})
+
+    useEffect(()=>{
+        fetch(`https://latest.currency-api.pages.dev/v1/currencies/${currency}.json`)
+        .then((res)=>res.json())
+        .then((res)=>setdata(res[currency]))
+        console.log(data)
+    },[currency])
+    console.log(data)
+    return data
+}
+
+export default usecurrencyinfo
